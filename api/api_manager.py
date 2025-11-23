30aca33189af67154fb240600123152a61d1ccb5
Gl <Dimadd11ace@yandex.ru>
2025-11-23 13:38:12 +0500

    Added movies_api & api_manager

---
+++ api/api_manager.py
@@ -0,0 +1,18 @@
+from requests import Session
+
+from movies_api import MoviesAPI
+
+
+class APIManager:
+    """
+    Класс для управления API-классами с единой HTTP-сессией.
+    """
+
+    def __init__(self, session: Session) -> None:
+        """
+        Инициализация APIManager.
+        :param session: HTTP-сессия, используемаявсеми API-классами
+        """
+        self.session = session
+
+        self.movies_api = MoviesAPI(session)
---
+++ api/movies_api.py
@@ -0,0 +1,38 @@
+from custom_requester.custom_requester import CustomRequester
+from requests import Session, Response
+
+from constants import BASE_URL, MOVIES_ENDPOINT
+
+
+class MoviesAPI(CustomRequester):
+    """
+    Класс для работы с Movies API
+    """
+
+    def __init__(self, session: Session) -> None:
+        super().__init__(base_url=BASE_URL, session=session)
+        self.session = session
+
+    def get_movie_info(self, movie_id: int, expected_status: int = 200) -> Response:
+        """
+        Получение информации о фильме.
+        :param movie_id: ID фильма.
+        :param expected_status: Ожидаемый статус-код.
+        """
+        return self.send_request(
+            method="GET",
+            endpoint=f"{MOVIES_ENDPOINT}/{movie_id}",
+            expected_status=expected_status,
+        )
+
+    def delete_movie(self, movie_id: int, expected_status: int = 200) -> Response:
+        """
+        Удаление фильма.
+        :param movie_id: ID фильма.
+        :param expected_status: Ожидаемый статус-код.
+        """
+        return self.send_request(
+            method="DELETE",
+            endpoint=f"{MOVIES_ENDPOINT}/{movie_id}",
+            expected_status=expected_status,
+        )
